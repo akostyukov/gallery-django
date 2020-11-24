2487b5f62a62bb1f25d8a6bd2b74cc00b30f4690
Artem Kostyukov <apkostyukov@gmail.com>
2020-11-24 15:05:09 +0300

    added gunicorn, nginx and some changes

--- src/gallery/settings.py
+++ src/gallery/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/images/'
 LOGOUT_REDIRECT_URL = '/images/'
