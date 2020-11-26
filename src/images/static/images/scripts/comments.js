d5fc8e36398067ed433d63c379ca011caefa7890
Artem Kostyukov <apkostyukov@gmail.com>
2020-11-26 17:45:25 +0300

    some edits

--- src/images/static/images/scripts/comments.js
+++ src/images/static/images/scripts/comments.js
@@ -1,14 +1,15 @@
-var form = $('#add-form')
-form.submit(function(){
-    $.ajax({
-            url: form.attr('action'),
-            type: 'get',
-            success: function(response){
-              alert(response);
-            },
-            error: function(rs, r){
-              alert("Произошла ошибка");
-            }
+$(document).ready(function(){
+    $('.comments').click(function(){
+        var commentsField = $(this).find('.comments-block')
+        $.ajax({
+                url: $(this).attr('name')+'/',
+                type: 'get',
+                success: function(response){
+                  comments.text(response.comments)
+                },
+                error: function(rs, r){
+                  alert("Произошла ошибка");
+                }
+        });
     });
-    return false;
 });
